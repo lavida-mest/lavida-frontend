f227049dd25d5b969d1f7380ffdb1996353fba4e
muathendirangu <muathe.ndirangu@gmail.com>
2020-03-09 13:15:49 +0000

    choose stage via radio button

--- scripts/stage.js
+++ scripts/stage.js
@@ -1,23 +1,9 @@
-
 let form =  document.querySelector('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let stage = new FormData(form);
-    console.log(stage)
-    for (choice in stage){
-        console.log(choice[1])
+    for (const choice of stage){
+        localStorage.setItem('stage', choice[1]);
     }
-    // const tripIdeas = document.getElementById('talk-type-1').value;
-    // const planTrip  = document.getElementById('talk-type-2').value;
-    // const bookTrip = document.getElementById('talk-type-1').value;
-    
-    // if(tripIdeas != '' || planTrip != '' || bookTrip != ''){
-
-    // }
-
-    // localStorage.setItem('trip-ideas', tripIdeas);
-    // localStorage.setItem('plan-trip', planTrip);
-    // localStorage.setItem('book-trip', bookTrip);
     console.log('successful');
-    alert('clicked')
 });
