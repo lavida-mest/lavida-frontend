a060c5f016a8c5fbe9253f312e67aaf9f0b6a058
muathendirangu <muathe.ndirangu@gmail.com>
2020-03-11 00:29:07 +0000

    store trips fetched to localstorage

---
+++ scripts/itinerary.js
@@ -0,0 +1,25 @@
+let form = document.querySelector('.traveler-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let itenary = new FormData(form);
+    for (choice of itenary) {
+        localStorage.setItem(choice[0], choice[1])
+        console.log(choice[0] + choice[1])
+    }
+});
+
+const getTrips = async () => {
+    let response =  await fetch("http://127.0.0.1:8080/trip", {
+        method:'GET',
+        headers: new Headers(
+            {
+                'Content-Type': 'application/json',
+            }
+        )
+    });
+    data = await response.json();
+    localStorage.setItem("trips", data);
+    console.log(data);
+}
+
+window.onload = getTrips();
