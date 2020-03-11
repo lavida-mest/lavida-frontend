5ecf69daf6a4cde4c8730bb4f45296046f53e981
muathendirangu <muathe.ndirangu@gmail.com>
2020-03-11 14:43:23 +0000

    add trip card template

--- scripts/itinerary.js
+++ scripts/itinerary.js
@@ -17,10 +17,47 @@ const getTrips = async () => {
             }
         )
     });
-    data = await response.json();
-    localStorage.setItem("trips", JSON.stringify(data));
+    trips = await response.json();
+
+    return trips.map((trip) => {
+        
+    });
+
+
+
+
+
+    localStorage.setItem("trips", JSON.stringify(trips));
     trips = JSON.parse(localStorage.getItem("trips"));
     console.log(trips[1].trip_activity);
 }
 
 window.onload = getTrips();
+
+
+
+const createNode = (element) => {
+    return document.createElement(element);
+}
+
+
+const append = (parent, el) => {
+    return parent.appendChild(el);
+}
+
+
+
+{/* <div class="col-md-4 ftco-animate">
+    <div class="project-wrap">
+       <a href="#" class="img" style="background-image: url(images/destination-1a.png);"></a>
+       		<div class="text p-4">
+       			<span class="price">Sightntrips</span>
+       			<span class="days">3 Days Tour</span>
+       			<h3><a href="#">Busia Weekend Escape</a></h3>
+        		<p class="location"><span class="ion-ios-map"></span>Sekondi-Takoradi</p>
+        		<ul>
+        	    	<li><span class="flaticon-sun-umbrella"></span>Near Beach</li>
+            	</ul>
+        	</div>
+    </div>
+</div> */}
